f45c6746f794b8ac7acd965563e8a9f8935b704a
valiclud <valicek@atlas.cz>
2024-05-09 16:53:02 +0200

    Adding author to db script.

--- OldLanguages/database/createdb.sql
+++ OldLanguages/database/createdb.sql
@@ -3,7 +3,7 @@ SET FOREIGN_KEY_CHECKS=0;
 DROP TABLE IF EXISTS `original_text`;
 CREATE TABLE `original_text` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `author` varchar(255) NOT NULL,
+  `author_text` varchar(255) NOT NULL,
   `title` varchar(255) NOT NULL,
   `text` longtext NOT NULL,
   `text_img` longblob  NULL,
@@ -18,12 +18,12 @@ CREATE TABLE `original_text` (
 DROP TABLE IF EXISTS `translated_text`;
 CREATE TABLE `translated_text` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `author` varchar(255),
   `title` varchar(255),
   `text` longtext,
   `language` varchar(255),
   `insert_date` date,
   `revision` int,
+  `author_id` varchar(255),
   `original_text_id` int(11),
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
@@ -57,6 +57,7 @@ CREATE TABLE `author` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
+  `permission` int(2),
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
@@ -72,6 +73,8 @@ CREATE TABLE `audit` (
 
 alter table `translated_text`
     add foreign key (original_text_id) references original_text(id) on delete set null;
+alter table `translated_text`
+    add foreign key (author_id) references author(id) on delete set null;
 alter table `original_text`
     add foreign key (old_language_id) references old_language(id) on delete set null;
 alter table `original_text`
--- OldLanguages/database/insertdb.sql
+++ OldLanguages/database/insertdb.sql
@@ -8,11 +8,11 @@ VALUES (3 /*not nullable*/,'Egyptian' /*not nullable*/,'light' /*not nullable*/)
 INSERT INTO old_language (id,language,period)
 VALUES (4 /*not nullable*/,'Egyptian Kopt' /*not nullable*/,'light' /*not nullable*/);
 
-INSERT INTO original_text (id,author,title,text,text_img,century,insert_date,hits,place_id,old_language_id)
+INSERT INTO original_text (id,author_text,title,text,text_img,century,insert_date,hits,place_id,old_language_id)
 VALUES (2 /*not nullable*/,'s' /*not nullable*/,'s' /*not nullable*/,'s' /*not nullable*/,'s',0 /*not nullable*/,{d '2023-12-04'} /*not nullable*/,0 /*not nullable*/,1 /*not nullable*/,1 /*not nullable*/);
-INSERT INTO original_text (id,author,title,text,text_img,century,insert_date,hits,place_id,old_language_id)
+INSERT INTO original_text (id,author_text,title,text,text_img,century,insert_date,hits,place_id,old_language_id)
 VALUES (4 /*not nullable*/,'t' /*not nullable*/,'t' /*not nullable*/,'s' /*not nullable*/,'t',0 /*not nullable*/,{d '2023-12-04'} /*not nullable*/,0 /*not nullable*/,2 /*not nullable*/,2 /*not nullable*/);
-INSERT INTO original_text (id,author,title,text,text_img,century,insert_date,hits,place_id,old_language_id)
+INSERT INTO original_text (id,author_text,title,text,text_img,century,insert_date,hits,place_id,old_language_id)
 VALUES (7 /*not nullable*/,'u' /*not nullable*/,'u' /*not nullable*/,'u' /*not nullable*/,'s',0 /*not nullable*/,{d '2023-12-04'} /*not nullable*/,0 /*not nullable*/,3 /*not nullable*/,3 /*not nullable*/);
 
 INSERT INTO place (id,place,country)
@@ -22,12 +22,15 @@ VALUES (2 /*not nullable*/,'Cyprus' /*not nullable*/,'Greek' /*not nullable*/);
 INSERT INTO place (id,place,country)
 VALUES (3 /*not nullable*/,'Athens' /*not nullable*/,'Greek' /*not nullable*/);
 
-INSERT INTO translated_text (id,author,title,text,language,insert_date,revision,original_text_id) 
-VALUES (2 /*not nullable*/,'s','s','s','s',{d '2023-12-04'},0,1 /*not nullable*/);
-INSERT INTO translated_text (id,author,title,text,language,insert_date,revision,original_text_id)
-VALUES (1 /*not nullable*/,'s','s','s','s',{d '2023-12-04'},0,1 /*not nullable*/);
-INSERT INTO translated_text (id,author,title,text,language,insert_date,revision,original_text_id)
-VALUES (4 /*not nullable*/,'tt','tt','tt','s',{d '2023-12-04'},0,2 /*not nullable*/);
+INSERT INTO author (id,email,passw, permission)
+VALUES (1 /*not nullable*/,'root@root.cz' /*not nullable*/,'root' /*not nullable*/, 64);
+
+INSERT INTO translated_text (id,title,text,language,insert_date,revision,author_id,original_text_id) 
+VALUES (2 /*not nullable*/,'s','s','s','s',{d '2023-12-04'},0,1,1 /*not nullable*/);
+INSERT INTO translated_text (id,title,text,language,insert_date,revision,author_id,original_text_id)
+VALUES (1 /*not nullable*/,'s','s','s','s',{d '2023-12-04'},0,1, 1 /*not nullable*/);
+INSERT INTO translated_text (id,title,text,language,insert_date,revision,author_id,original_text_id)
+VALUES (4 /*not nullable*/,'tt','tt','tt','s',{d '2023-12-04'},0,1, 2 /*not nullable*/);
 
 INSERT INTO pagination (id,controller_name,results)
 VALUES ( 1/*not nullable*/,'translatedtextController' /*not nullable*/,3 /*not nullable*/);
