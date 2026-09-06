d7875c926fa7d78562a03900a71eb976358b2bcd
valiclud <valicek@atlas.cz>
2026-09-06 12:10:28 +0200

    Correcting OriginalTextDBTest.php

--- OldLanguages/tests/unit/OriginalTextDBTest.php
+++ OldLanguages/tests/unit/OriginalTextDBTest.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 
 use PHPUnit\Framework\TestCase;
@@ -16,15 +16,15 @@ final class OriginalTextDBTest extends TestCase
     private ?DatabaseTable $authorsTable;
     private ?DatabaseTable $paginationTable;
     private ?Authentication $authentication;
-  
 
-    public function setUp() : void
+
+    public function setUp(): void
     {
         try {
             $pdo = new \PDO('mysql:host=localhost:3306;dbname=old_languages;charset=utf8mb4', 'root', 'lukasa');
             $this->placesTable = new DatabaseTable($pdo, 'place', 'id', '\entities\Place');
             $this->languageTable = new DatabaseTable($pdo, 'old_language', 'id', '\entities\OldLanguage');
-            $this->originalTextTable = new DatabaseTable($pdo, 'original_text', 'id', '\entities\OriginalText', [&$this->placesTable, &$this->languageTable]);
+            $this->originalTextTable = new DatabaseTable($pdo, 'original_text', 'id', '\entities\OriginalText', [&$this->placesTable, &$this->languageTable, &$this->authorsTable]);
             $this->translatedTextTable = new DatabaseTable($pdo, 'translated_text', 'id', '\entities\TranslatedText', [&$this->originalTextTable]);
             $this->authorsTable = new DatabaseTable($pdo, 'author', 'id', '\entities\Author');
             $this->paginationTable = new DatabaseTable($pdo, 'pagination', 'id', '\entities\Pagination');
@@ -32,41 +32,47 @@ final class OriginalTextDBTest extends TestCase
         } catch (\PDOException $e) {
             echo "<script>console.log('$e');</script>";
             echo "$e";
-            //throw new \PDOException($e);
+            throw new \PDOException($e);
         }
     }
 
     public function test_insert_original_text_to_db(): void
     {
-        $originalText['author'] = "Unknown";
+        $originalText['author_text'] = "Unknown";
         $originalText['title'] = "Battle xxxl";
         $originalText['text'] = "And so on...";
-        $originalText['century'] = '1 B.C.';
+        $originalText['century'] = 1;
         $originalText['insert_date'] = date_create()->format('Y-m-d');
         $originalText['hits'] = 2;
         $originalText['place_id'] = 1;
         $originalText['old_language_id'] = 2;
+        $originalText['author_id'] = 1;
 
         $savedId = $this->originalTextTable->save($originalText);
         $originalTextDB = $this->originalTextTable->find('id', $savedId)[0] ?? null;
 
         $this->assertNotNull($originalTextDB);
-        $this->assertSame($originalTextDB->author_text,"Unknown");
-        $this->assertSame($originalTextDB->title,"Battle xxxl");
-        $this->assertSame($originalTextDB->text,"And so on...");
-        $this->assertSame($originalTextDB->century,'1 B.C.');
+        $this->assertSame($originalTextDB->author_text, "Unknown");
+        $this->assertSame($originalTextDB->title, "Battle xxxl");
+        $this->assertSame($originalTextDB->text, "And so on...");
+        $this->assertSame($originalTextDB->century, 1);
         $this->assertSame($originalTextDB->insert_date, date_create()->format('Y-m-d'));
-        $this->assertSame($originalTextDB->hits,2);
-        $this->assertSame($originalTextDB->place_id,1);
-        $this->assertSame($originalTextDB->old_language_id,2);
+        $this->assertSame($originalTextDB->hits, 2);
+        $this->assertSame($originalTextDB->place_id, 1);
+        $this->assertSame($originalTextDB->old_language_id, 2);
+        $this->assertSame($originalTextDB->author_id, 1);
 
         $this->originalTextTable->delete('id', $savedId);
     }
 
-    public function tearDown() : void
+    public function tearDown(): void
     {
-        $pdo = null;
+        try {
+            $pdo = null;
+        } catch (\PDOException $e) {
+            echo "$e";
+            error_log($e->getMessage());
+            throw new \PDOException($e);
+        }
     }
-
-
 }
