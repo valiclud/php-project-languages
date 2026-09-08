e4053f0920297e83daf12d44207aba24e404e3e1
valiclud <valicek@atlas.cz>
2026-09-08 11:09:13 +0200

    Gitignore

--- OldLanguages/classes/Website.php
+++ OldLanguages/classes/Website.php
@@ -1,6 +1,9 @@
 <?php
+
 namespace classes;
-interface Website {
+
+interface Website
+{
     public function getDefaultRoute(): string;
     public function getController(string $controllerName): ?object;
     public function checkLogin(string $uri): ?string;
--- OldLanguages/controllers/api/OriginalTextApiController.php
+++ OldLanguages/controllers/api/OriginalTextApiController.php
@@ -15,8 +15,7 @@ class OriginalTextApiController extends BaseApiController
 		private \classes\DatabaseTable $paginationTable,
 		private \classes\Authentication $authentication,
 		private \classes\DatabaseTable $authorsTable
-	) {
-	}
+	) {}
 
 	public function delete($id = null)
 	{
@@ -26,13 +25,13 @@ class OriginalTextApiController extends BaseApiController
 	}
 
 	public function list(?int $page = 1)
-	{	
+	{
 		$pagination = $this->paginationTable->find('controller_name', 'apiOriginalTextController')[0];
 		if ($pagination == null) {
 			$message = 'Record column controller_name -> "apiOriginalTextController" is not stored in database table pagination
 			default value pagination=5 is to be set.';
 			$pagination = Pagination::default();
-            error_log($message);
+			error_log($message);
 		}
 
 		$limit = $pagination->results;
@@ -40,10 +39,15 @@ class OriginalTextApiController extends BaseApiController
 		$originalTexts = $this->originalTextTable->findAll($limit, $offset);
 		$totaloriginalTexts = $this->originalTextTable->total();
 		$totalPages = ceil($totaloriginalTexts / $pagination->results);
-		$data = array("total" => $totaloriginalTexts, "total_pages" => $totalPages, "per_page" => $pagination->results, 
-			"page_number" => $page, "data" => $originalTexts);
+		$data = array(
+			"total" => $totaloriginalTexts,
+			"total_pages" => $totalPages,
+			"per_page" => $pagination->results,
+			"page_number" => $page,
+			"data" => $originalTexts
+		);
 		$responseData = json_encode($data);
-	
+
 		$this->sendOutput($responseData, array("Content-Type: application/json", "HTTP/1.1 200 OK", "Access-Control-Allow-Origin: *"));
 
 		return null;
@@ -64,8 +68,8 @@ class OriginalTextApiController extends BaseApiController
 		return null;
 	}
 
-    public function post($id = null, $array = null)
-    {
+	public function post($id = null, $array = null)
+	{
 
 		/*dat do hlavicky:
 		Accept: application/json
@@ -88,16 +92,16 @@ Access-Control-Allow-Headers: *
 		//$this->sendOutput(null, array('Content-Type: application/json', "HTTP/1.1 200 OK", "Access-Control-Allow-Origin: *"));
 		$this->sendOutput($responseData, array('Content-Type: application/json',  "HTTP/1.1 200 OK", "Access-Control-Allow-Origin: *"));
       	*/
-		  $title = 'Edit Original Text';
+		$title = 'Edit Original Text';
 
-		  return [
-			  'template' => 'frontend_api/editoriginaltext.html',
-			  'title' => $title,
-			  'variables' => [
-				  'originalText' => null
-			  ]
-		  ];
-        /*
+		return [
+			'template' => 'frontend_api/editoriginaltext.html',
+			'title' => $title,
+			'variables' => [
+				'originalText' => null
+			]
+		];
+		/*
         if (! $this->validatePerson($input)) {
             return $this->unprocessableEntityResponse();
         }
@@ -109,9 +113,11 @@ Access-Control-Allow-Headers: *
     $user->username = $post['username'];
     echo json_encode($user);
         */
-    }
+	}
+// curl.exe -X POST http://localhost/originaltextapi/post -H "Content-Type: application/json" -H "Accept: application/json" -d "{\"origtextauthor\":\"Unknown\",\"origtexttitle\":\"Battle xxxl\",\"origtexttext\":\"And so on...\",\"origtextimage\":\"\",\"origtextcentury\":1,\"idplace\":1,\"idlanguage\":2,\"idauthor\":1}"
 
-	public function postSubmit() {
+	public function postSubmit()
+	{
 		if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
 			header('Access-Control-Allow-Origin: *');
 			header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
@@ -123,33 +129,40 @@ Access-Control-Allow-Headers: *
 		header('Content-Type: application/json');
 
 		$post = json_decode(file_get_contents('php://input'), true);
-		error_log(json_encode($post));
-		error_log($post['origtextauthor']);
-	/*	
-		$origText = OriginalText::default($this->placesTable, $this->languageTable);
-		$origText->setAuthor($post['origtextauthor']);
-		$origText->setText($post['origtexttext']);
-		$origText->setTitle($post['origtexttitle']);
-		$origText->setTextimg($post['origtextimage']);
-		$origText->setCentury($post['origtextcentury']);
-		$origText->setInsertdate(date_create()->format('Y-m-d'));
-		$origText->setHits(1);
-*/
-		$origText = array();
-		$origText['author_text'] = $post['origtextauthor'];
-		$origText['text'] = $post['origtexttext'];
-		$origText['title'] = $post['origtexttitle'];
-		$origText['text_img'] = $post['origtextimage'];
-		$origText['century'] = $post['origtextcentury'];
-		$origText['insert_date'] = date_create()->format('Y-m-d');
-		$origText['hits'] = 1;
-		$origText['place_id'] = $post['idplace'];
-		$origText['old_language_id'] = $post['idlanguage'];
+		$requiredFields = [
+			'origtextauthor',
+			'origtexttext',
+			'origtexttitle',
+			'origtextimage',
+			'origtextcentury',
+			'idplace',
+			'idlanguage',
+			'idauthor'
+		];
 
-		$this->originalTextTable->save($origText);
-	
+		if (!is_array($post) || array_diff($requiredFields, array_keys($post))) {
+			http_response_code(400);
+			echo json_encode(['error' => 'Invalid request body.']);
+			return null;
+		}
+
+		$originalText = [
+			'author_text' => $post['origtextauthor'],
+			'text' => $post['origtexttext'],
+			'title' => $post['origtexttitle'],
+			'text_img' => $post['origtextimage'],
+			'century' => (int) $post['origtextcentury'],
+			'insert_date' => date_create()->format('Y-m-d'),
+			'hits' => 1,
+			'place_id' => (int) $post['idplace'],
+			'old_language_id' => (int) $post['idlanguage'],
+			'author_id' => (int) $post['idauthor']
+		];
+
+		$id = $this->originalTextTable->save($originalText);
+		http_response_code(201);
+		echo json_encode(['id' => $id]);
+
 		return null;
-
 	}
-
 }
